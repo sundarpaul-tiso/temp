0348cd11376258074c59da9d1040af98ccde4403
sundarpaul-tiso <sundarpaul.d@tisostudio.com>
2025-05-07 13:53:20 +0530

    Update file.ts

--- file.ts
+++ file.ts
@@ -1,38 +1,42 @@
-const getActiveIndicator = (currentLeg: any) => {
-    const legIndex: any = data?.findIndex((leg: any) => leg.name == currentLeg);
-    const leg = data[legIndex];
+const getActiveIndicator = (currentLeg: any): boolean => {
+  const legIndex = data?.findIndex((leg: any) => leg.name === currentLeg);
+  const leg = data?.[legIndex];
 
-    if (currentLeg == TripStatus.DriverStartedForPickup && leg?.fulfilled) {
-      const inTransit = data?.findIndex(
-        (leg: any) => leg?.name == TripStatus.InTransit && leg?.fulfilled
-      );
-      return inTransit == -1;
-    } else if (currentLeg == TripStatus.InTransit && leg?.fulfilled) {
-      const truckOffLoaded = data?.findIndex(
-        (leg: any) => leg?.name == TripStatus.TruckOffLoaded && leg?.fulfilled
-      );
-      return truckOffLoaded == -1;
-    } else if (currentLeg == TripStatus.Delivered && !leg?.fulfilled) {
-      const truckOffLoaded = data?.findIndex(
-        (leg: any) => leg?.name == TripStatus.TruckOffLoaded && leg?.fulfilled
-      );
+  const isFulfilled = (status: TripStatus) =>
+    data?.some((leg: any) => leg.name === status && leg.fulfilled);
 
-      return truckOffLoaded !== -1;
-    } else if (currentLeg == TripStatus.DriverAssigned) {
-      const carrierAccepted = data?.findIndex(
-        (l: any) => l?.name == TripStatus.CarrierAccepted && l?.fulfilled
-      );
-      if (!leg?.fulfilled && carrierAccepted !== -1) {
-        return true;
-      } else {
-        const carrierAccepted = data?.findIndex(
-          (l: any) => l?.name == TripStatus.CarrierAccepted && l?.fulfilled
-        );
-        const startedForPickup = data?.findIndex(
-          (leg: any) =>
-            leg?.name == TripStatus.DriverStartedForPickup && !leg?.fulfilled
-        );
-        return startedForPickup !== -1 && carrierAccepted !== -1;
+  const isUnfulfilled = (status: TripStatus) =>
+    data?.some((leg: any) => leg.name === status && !leg.fulfilled);
+
+  switch (currentLeg) {
+    case TripStatus.DriverStartedForPickup:
+      if (leg?.fulfilled) {
+        return !isFulfilled(TripStatus.InTransit);
       }
-    }
-  };
+      break;
+
+    case TripStatus.InTransit:
+      if (leg?.fulfilled) {
+        return !isFulfilled(TripStatus.TruckOffLoaded);
+      }
+      break;
+
+    case TripStatus.Delivered:
+      if (!leg?.fulfilled) {
+        return isFulfilled(TripStatus.TruckOffLoaded);
+      }
+      break;
+
+    case TripStatus.DriverAssigned:
+      const carrierAcceptedFulfilled = isFulfilled(TripStatus.CarrierAccepted);
+      const startedForPickupUnfulfilled = isUnfulfilled(TripStatus.DriverStartedForPickup);
+
+      if (!leg?.fulfilled && carrierAcceptedFulfilled) {
+        return true;
+      }
+
+      return carrierAcceptedFulfilled && startedForPickupUnfulfilled;
+  }
+
+  return false;
+};
